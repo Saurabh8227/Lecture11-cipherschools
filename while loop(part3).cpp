401e778ac5f5272398cf606cf94505efc5ba78eb
Saurabh Pandey <119298223+Saurabh8227@users.noreply.github.com>
2024-06-23 12:47:21 +0530

    Add files via upload

---
+++ while loop(part3).cpp
@@ -0,0 +1,29 @@
+/*
+1
+#include<iostream>
+using namespace std;
+int main(){
+	int i = 1;
+	while(i<101){
+		cout<<i<<" ";
+		i++;
+	}
+}*/
+
+//// use of Break statement
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	i=1000;
+	int input;
+	for(i=1;i<101,i++){
+		cin>>input;
+		if(input== 60){
+			cout<<"congrats you are upgraded to personality: ";
+			bfreak;
+		}
+	}
+	
+}
+
